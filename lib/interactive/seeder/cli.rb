762bf18d0da0f99f7b61e0748943c484bea5eaec
Martin Spicuzza <spicuzza157@gmail.com>
2023-12-04 10:54:12 -0600

    adding seeder boilerplate

---
+++ lib/interactive/seeder/cli.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "cli/version"
+
+module Interactive
+  module Seeder
+    module Cli
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
